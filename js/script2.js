b0f6b45332bc1f036848d254b16ade0e3db2e1e6
Oleh <baryliak.oleh@gmail.com>
2021-11-18 16:42:19 +0200

    вчив ту ху*ню

---
+++ js/script2.js
@@ -0,0 +1,28 @@
+"use sctrict";
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = '5';
+
+const bool = false;
+
+// console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMerried: false
+};
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+
+let arr = ['plum.png', 6, {}, [], 'orange.jpg', 'apple.bmp'];
+console.log(arr[1]);
